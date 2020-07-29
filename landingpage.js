0967b2ed8b31876b217ee8cd0c146bd6c62981d3
UneSaltedFish <65207730+UneSaltedFish@users.noreply.github.com>
2020-07-29 12:30:58 -0400

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,25 @@
+
+
+
+
+function circle2(){
+  document.getElementById("circle").src = "circle1.png";
+}
+function circle1(){
+  document.getElementById("circle").src = "circle.png";
+}
+function right2(){
+  document.getElementById("right").src = "right2.png";
+  document.getElementById("right1").style.opacity=1;
+  document.getElementById("right2").style.opacity=1;
+}
+function right1(){
+  document.getElementById("right").src = "right.png";
+  document.getElementById("right1").style.opacity=0;
+  document.getElementById("right2").style.opacity=0;
+}
+
+//document.getElementById("circle").src = "backgroundPic.png";
+//document.getElementById("circle").src = "backgroundPic.png";
+//document.getElementById("circle").src = "backgroundPic.png";
+//document.getElementById("circle").src = "backgroundPic.png";
