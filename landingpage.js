7b697d8a5dee096e928599283a5ce9cdb769e5f7
UneSaltedFish <65207730+UneSaltedFish@users.noreply.github.com>
2020-07-31 18:39:13 -0400

    Add files via upload

---
+++ landingpage.js
@@ -0,0 +1,42 @@
+
+
+
+
+function circle2(){
+  document.getElementById("circle").src = "pictures/circle1.png";
+}
+function circle1(){
+  document.getElementById("circle").src = "pictures/circle.png";
+}
+function right2(){
+  document.getElementById("right").src = "pictures/right2.png";
+  document.getElementById("right1").style.opacity=1;
+  document.getElementById("right2").style.opacity=1;
+}
+function right1(){
+  document.getElementById("right").src = "pictures/right.png";
+  document.getElementById("right1").style.opacity=0;
+  document.getElementById("right2").style.opacity=0;
+}
+function bottom2(){
+  document.getElementById("bottom").src = "pictures/bottom2.png";
+  document.getElementById("bottomtext").style.opacity=1;
+
+}
+function bottom1(){
+  document.getElementById("bottom").src = "pictures/bottom.png";
+  document.getElementById("bottomtext").style.opacity=0;
+
+}
+function above2(){
+  document.getElementById("above").src = "pictures/above2.png";
+}
+function above1(){
+  document.getElementById("above").src = "pictures/above.png";
+}
+function left2(){
+  document.getElementById("left").src = "pictures/left2.png";
+}
+function left1(){
+  document.getElementById("left").src = "pictures/left.png";
+}
